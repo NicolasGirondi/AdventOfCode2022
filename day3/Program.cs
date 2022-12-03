111cb1f01be8e6c588754987a3c5f5fd7d04a4d6
lente1 <nicolas.girondi@gmail.com>
2022-12-03 12:07:24 +0100

    Add a function to reuse code, Added solution for part 2

--- day3/Program.cs
+++ day3/Program.cs
@@ -24,20 +24,53 @@
     }
     static void part2()
     {
-
+        StreamReader sr = new StreamReader("input.txt");
+        string s = sr.ReadToEnd();//read all the file
+        sr.Close();//close the file
+        string[] rucksacks = s.Split('\n');//split the rows in rucksacks
+        string[] elfgroup = new string[3];//every group is composed of 3 elves
+        int sum = 0;
+        for (int i = 0; i < rucksacks.Length / 3; i++)//for every elfgroup
+        {
+            for (int j = 0; j < elfgroup.Length; j++)//for every elf of the group
+            {
+                elfgroup[j] = rucksacks[3 * i + j];//fill the group with the rucksacks
+            }
+            sum += grouppriority(elfgroup);
+        }
+        StreamWriter sw = new StreamWriter("output.txt");
+        sw.WriteLine(sum);//write the output
+        sw.Close();//close the file
     }
-    static int rucksackpriority(string[] compartmets)
+    static int rucksackpriority(string[] compartments)
     {//calculates what item type is in both the compartments and returns the item priority
         int res = 0, i = 0;
         bool found = false;
         do//while no item is repeated in the compartments and the compartment is not completely checked
         {
-            found = compartmets[1].Contains(compartmets[0][i]);//search if the item ot this compartment is in the other compartment
+            found = compartments[1].Contains(compartments[0][i]);//search if the item ot this compartment is in the other compartment
             if (found)
-                res = (compartmets[0][i] >= 'A' && compartmets[0][i] <= 'Z') ? compartmets[0][i] - 38 : compartmets[0][i] - 96;
+                res = itempriority(compartments[0][i]);
             //result = (from 1 through 26 for letters from 'a' through 'z', from 27 to 52 for letters from 'A' to 'Z')
             i++;
-        } while (!found && i < compartmets[0].Length);
+        } while (!found && i < compartments[0].Length);
         return res;
+    }
+    static int grouppriority(string[] elfgroup)
+    {//calculates what item type is in all the three groups and returns the item priority
+        int res = 0, i = 0;
+        bool found = false;
+        do//while no item is repeated in the three groups
+        {   //find the item that is in all the 3 group rucksacks
+            found = elfgroup[1].Contains(elfgroup[0][i]) && elfgroup[2].Contains(elfgroup[0][i]);
+            if (found)
+                res = itempriority(elfgroup[0][i]);//calculate the priority of the repeated item
+            i++;
+        } while (!found && i < elfgroup[0].Length);
+        return res;
+    }
+    static int itempriority(char item)
+    {
+        return (item >= 'A' && item <= 'Z') ? item - 38 : item - 96;
     }
 }
